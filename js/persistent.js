150efb478ae1f529cf9d54d590f4d685a7cc3717
manuelrbrizi <37914259+manuelrbrizi@users.noreply.github.com>
2019-05-09 11:44:39 -0300

    Add files via upload

---
+++ js/persistent.js
@@ -0,0 +1,40 @@
+new Vue({
+    el: '#app',
+    data () {
+      return {
+        items: [
+          { title: 'Home', icon: 'house', id: 'homeNavBar'},
+          { title: 'Rooms', icon: 'group_work', id: 'roomsNavBar' },
+          { title: 'Devices', icon: 'perm_device_information', id: 'devicesNavBar' },
+          { title: 'Routines', icon: 'alarm', id: 'routinesNavBar' },
+          { title: 'Active devices', icon: 'highlight', id: 'acdevNavBar' },
+        ],
+        right: null
+        }
+      }
+    })
+
+var home = document.getElementById("homeNavBar");
+home.onclick = function(){
+  location.replace("index.html");
+}
+
+var rooms = document.getElementById("roomsNavBar");
+rooms.onclick = function(){
+  location.replace("rooms.html");
+}
+
+var devices = document.getElementById("devicesNavBar");
+devices.onclick = function(){
+  location.replace("devices.html");
+}
+
+var routines = document.getElementById("routinesNavBar");
+routines.onclick = function(){
+  location.replace("routines.html");
+} 
+
+var actdev = document.getElementById("acdevNavBar");
+actdev.onclick = function(){
+  location.replace("active-devices.html");
+}
